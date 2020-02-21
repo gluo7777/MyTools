7c3532d2c793d50e1eaa015286f1431ff3f48d7d
Your Mom <gluo7777@gmail.com>
2020-02-20 18:03:58 -0600

    temp

--- cli/scripts/main.py
+++ cli/scripts/main.py
@@ -1,4 +1,6 @@
 import click
+import os
+import sys
 
 @click.group()
 @click.option('--verbose', is_flag=True, default=False, help="Log extravaneous output")
@@ -10,3 +12,16 @@ def root(context, verbose):
         click.echo("Unable to initialize CLI context due to following error: \n%s" % e)
     context.obj['VERBOSE'] = verbose
 
+@root.command('info')
+@click.pass_context
+def info(context):
+    if context['VERBOSE']:
+        click.secho("Verbosity is enabled.", fg='green')
+    click.echo("""Current Directory: %s\
+        \nCurrent Directory: %s\
+    """ % (
+        os.path.abspath('.'),
+        os.path.abspath('.')
+    ))
+    click.echo('Platform: %s' % os.name)
+    click.echo('File System Encoding: %s' % sys.getdefaultencoding())
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Development
+
+1. Version > Python 3.8
+2. Create setup.py
+3. Initialize virtualenv
+    > virtualenv env
+    > ./venv/Scripts/activate
