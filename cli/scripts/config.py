46ce74551be0f3b169dd067f969be58cdd36be0f
Your Mom <gluo7777@gmail.com>
2020-02-22 14:08:23 -0600

    added configuration properties

---
+++ cli/scripts/config.py
@@ -0,0 +1,50 @@
+import os
+from configparser import ConfigParser, ExtendedInterpolation
+from pathlib import Path
+import cli.scripts.context as context
+
+CONFIG_DIR = os.path.expanduser('~/mytool')
+CONFIG_FILE = os.path.abspath(CONFIG_DIR + '/' + 'config.ini')
+
+class Properties:
+
+    FAILED_ONCE = False
+
+    def __init__(self, section):
+        self.section = section
+        self.parser = ConfigParser(
+            allow_no_value=True,
+            comment_prefixes='#',
+            strict=True,
+            empty_lines_in_values=False,
+            interpolation=ExtendedInterpolation()
+        )
+
+        if not os.path.exists(CONFIG_FILE) and not Properties.FAILED_ONCE:
+            context.log(f"Creating config file at {CONFIG_FILE}")
+            try:
+                os.makedirs(CONFIG_DIR,exist_ok=True)
+                open(CONFIG_FILE, 'a').close()
+                self.parser.read(CONFIG_FILE)
+            except Exception as e:
+                context.log(f"Failed to create config file: {e}", error=True)
+                Properties.FAILED_ONCE = True
+
+        if not self.parser.has_section(self.section):
+            self.parser.add_section(section)
+    
+    def get(self, option, fallback=None):
+        self.parser.get(self.section, option, fallback=fallback)
+
+    def set(self, option, value, persist=False):
+        self.parser.set(self.section,option,value)
+        if persist:
+            context.debug('Persisting property {self.section}.{option}={value}')
+            try:
+                with open(CONFIG_FILE, mode='w') as fp:
+                    self.parser.write(fp)
+            except Exception as e:
+                context.log('Failed to persist properties: {e}', error=True)
+    
+    def has(self, option):
+        return self.parser.has_option(self.section, option)
--- cli/scripts/context.py
+++ cli/scripts/context.py
@@ -2,3 +2,10 @@ import click
 
 def is_debug():
     return click.get_current_context().obj['VERBOSE'] or False
+
+def log(msg,error=False):
+    click.echo(msg,err=error)
+
+def debug(msg):
+    if is_debug():
+        click.echo(msg)
