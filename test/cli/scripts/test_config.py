4704b727bdcbb1595cc6f6f2af1132079f71c898
Your Mom <gluo7777@gmail.com>
2020-03-06 19:15:53 -0600

    added more tests to test_config.py

--- test/cli/scripts/test_config.py
+++ test/cli/scripts/test_config.py
@@ -2,16 +2,39 @@ from cli.scripts.config import Properties
 import test
 import unittest
 import os
+from pathlib import Path
+from configparser import ExtendedInterpolation
 
 class PropertiesTest(unittest.TestCase):
 
     def setUp(self):
-        self.props = Properties('Config',CONFIG_DIR=test.RESOURCE_DIR)
+        self.props = Properties('Config',CONFIG_DIR=test.RESOURCE_DIR, CONFIG_FILE=f'propertiestest.ini')
 
-    def test_fields_not_null(self):
-        for k,v in self.props.__dict__.items():
-            if not str(k).startswith('__'):
-                self.assertIsNotNone(v,f'{k} is None')
+    def test_values(self):
+        self.assertEqual(self.props.CONFIG_DIR, test.RESOURCE_DIR)
+        self.assertEqual(self.props.CONFIG_FILE, os.path.abspath(test.RESOURCE_DIR + '/' + 'propertiestest.ini'))
+        self.assertFalse(self.props.FAILED_ONCE)
+        self.assertEqual(self.props._section, 'Config')
+        self.assertIsNotNone(self.props._parser)
+
+    def test_config_file_creation(self):
+        self.assertTrue(Path(self.props.CONFIG_FILE).is_file())
+        self.assertTrue(Path(self.props.CONFIG_FILE).exists())
+
+    def test_section_created(self):
+        self.assertTrue(self.props._parser.has_section('Config'))
+
+    def test_update_option(self):
+        key = 'my.prop'
+        val = 'abc'
+        val2 = 'def'
+        self.assertFalse(self.props.has(key))
+        self.props.set(key,val)
+        self.assertTrue(self.props.has(key))
+        self.assertEqual(self.props.get(key), val)
+        self.props.set(key,val2)
+        self.assertTrue(self.props.has(key))
+        self.assertEqual(self.props.get(key), val2)
 
     def tearDown(self):
         os.remove(self.props.CONFIG_FILE)
